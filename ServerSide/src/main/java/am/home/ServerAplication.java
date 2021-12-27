5d13dc6a2e81f660dc4fb0d9c167d2b8d697ff69
Inna <to_ne_to@mail.ru>
2021-12-27 19:22:06 +0300

    Домашнее задание к уроку 7 Java2 1 Разобраться с кодом. 2 Реализовать личные сообщения, если клиент пишет «/w nick3 Привет», то только клиенту с ником nick3 должно прийти сообщение «Привет».

---
+++ ServerSide/src/main/java/am/home/ServerAplication.java
@@ -0,0 +1,11 @@
+package am.home;
+
+import am.home.service.MyServer;
+
+public class ServerAplication {
+    public static void main(String[] args) {
+
+        new MyServer();
+
+    }
+}
---
+++ ServerSide/src/main/java/am/home/handler/ClientHandler.java
@@ -0,0 +1,126 @@
+package am.home.handler;
+
+import am.home.service.MyServer;
+
+import javax.swing.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+    private MyServer myServer;
+    private Socket socket;
+    private DataInputStream dis;
+    private DataOutputStream dos;
+
+    private String nickName;
+
+    public ClientHandler(MyServer myServer, Socket socket) {
+
+        try {
+            this.myServer = myServer;
+            this.socket = socket;
+            this.dis = new DataInputStream(socket.getInputStream());
+            this.dos = new DataOutputStream(socket.getOutputStream());
+            this.nickName = "";
+            new Thread(() -> {
+                try {
+                    authentication();
+                    receive();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    closeConnection();
+                }
+            }).start();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Проблемы при создании обработчика клиента");
+        }
+    }
+
+    private void closeConnection() {
+        myServer.unsubscribe(this);
+        myServer.sendMessageToClients(nickName + " покинул(а) чат");
+        try {
+            dis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            dos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void authentication() throws Exception {
+
+        while (true) {
+            String message = dis.readUTF();
+            if (message.startsWith("/start")) {
+                String[] arr = message.split("-", 3);
+                if (arr.length != 3) {
+                    throw new IllegalAccessException();
+                }
+                final String nick = myServer
+                        .getAuthenticationService()
+                        .getNickNameByLoginPassword(arr[1].trim(), arr[2].trim());
+
+                if (nick != null) {
+                    if (!myServer.nickIsBusy(nick)) {
+                        send(nick + " в чате");
+                        this.nickName = nick;
+                        myServer.sendMessageToClients(nick + "зашел в чат");
+                        myServer.subscribe(this);
+                        return;
+                    } else {
+                        send("Ник занят");
+                    }
+                } else {
+                    send("Неверный логин и/или пароль");
+                }
+            }
+        }
+    }
+
+    public void send(String message) {
+        try {
+            dos.writeUTF(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void receive() throws IOException {
+        while (true) {
+            String message = dis.readUTF();
+            if (message.startsWith("/")) {
+                if (message.startsWith("/finish")) {
+                    myServer.sendMessageToClients(nickName + " вышел из чата");
+                    return;
+                }
+                if(message.startsWith("/w")) {
+                String to = message.split("-", 3)[1];
+                String msg = message.split("-", 3)[2];
+                myServer.sendMessageToClient(this, to, msg);
+                }
+                if(message.startsWith("/list")){
+                    myServer.onLineUsers(this);
+                }
+                continue;
+            }
+                myServer.sendMessageToClients(nickName + ": " + message);
+            }
+        }
+
+        public String getNickName () {
+            return nickName;
+        }
+    }
