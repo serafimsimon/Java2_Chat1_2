1cae30958c2ba72f720a7431cbfbc9339c543a3d
Inna <to_ne_to@mail.ru>
2022-02-03 14:05:15 +0300

    Домашнее задание к уроку 3 Java3
    1. Добавить в сетевой чат авторизацию через базу данных SQLite.

--- ServerSide/src/main/java/am/home/service/interfaces/AuthenticationServiceImpl.java
+++ ServerSide/src/main/java/am/home/service/interfaces/AuthenticationServiceImpl.java
@@ -1,56 +1,89 @@
 package am.home.service.interfaces;
 
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
 
-    private class UserEntity {
+    private static Connection connection;
+    private static PreparedStatement pr;
+
+    public static class UserEntity {
+
+        private int id;
         private String login;
         private String password;
         private String nickName;
 
+
         public UserEntity(String login, String password, String nickName) {
+
             this.login = login;
             this.password = password;
             this.nickName = nickName;
         }
     }
+
     private List<UserEntity> userEntityList;
 
-    @Override
-    public void start() {
+
+    public void start() throws SQLException {
+
         System.out.println("Сервис аутентификации запущен");
+
     }
 
-    @Override
+
     public void stop() {
+
         System.out.println("Сервис аутентификации остановлен");
+        disconnect();
 
     }
 
 
-    public AuthenticationServiceImpl() {
-        this.userEntityList = new ArrayList<>();
-        userEntityList.add(new UserEntity("A", "A", "A"));
-        userEntityList.add(new UserEntity("B", "B", "B"));
-        userEntityList.add(new UserEntity("C", "C", "C"));
+    public void disconnect() {
+        try {
+            if (pr != null) {
+                pr.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
-    @Override
+    public AuthenticationServiceImpl() throws SQLException {
+
+              connection = DriverManager.getConnection("jdbc:sqlite:users.db");
+              pr = connection.prepareStatement("SELECT * FROM users;");
+              ResultSet rs = pr.executeQuery();
+
+              this.userEntityList = new ArrayList<>();
+
+            while (rs.next()) {
+
+                userEntityList.add(new UserEntity(rs.getString("login"), rs.getString("password"), rs.getString("nickName")));
+
+            }
+    }
+
     public String getNickNameByLoginPassword(String login, String password) {
-        for (UserEntity entity: userEntityList){
-            if(entity.login.equals(login) && entity.password.equals(password))
-                return entity.nickName;
-                   }
+
+        for (UserEntity user : userEntityList) {
+            if (user.login.equals(login) && user.password.equals(password))
+                return user.nickName;
+        }
         return null;
-
+    }
 }
-
-}
-
-
 
